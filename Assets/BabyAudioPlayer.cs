ed0b18b12b95bdc54f5b915c0de79a886c30fbbd
Juho Salmio <juho.salmio@gmail.com>
2015-02-24 16:35:12 +0200

    add baby crying and periodical hungry effect to the ralf-scene

---
+++ Assets/BabyAudioPlayer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BabyAudioPlayer : MonoBehaviour {
+	public AudioSource crying;
+	public AudioSource hungry;
+	bool keepPlaying = true;
+	int hungryInterval = 20;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(SoundOut());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator SoundOut()
+	{	
+		while (keepPlaying){
+			crying.Pause();
+			hungry.PlayOneShot(hungry.clip);
+			StartCoroutine(CryingResume(hungry.clip.length));
+			yield return new WaitForSeconds(hungryInterval + hungry.clip.length);
+		}
+	}
+
+	IEnumerator CryingResume(float timeToWait)
+	{	
+		yield return new WaitForSeconds(timeToWait);
+		crying.Play();
+	}
+}
