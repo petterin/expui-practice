6e5b9b65f2fb9655a722e01c85a5a77bf1c26037
Juho Salmio <juho.salmio@gmail.com>
2015-02-24 19:31:44 +0200

    add collision detector and sound to baby's mouth

---
+++ Assets/BabyCollisionAudioPlayer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BabyAudioPauser : MonoBehaviour {
+
+	public GameObject objectToTrigger1;
+	public GameObject objectToTrigger2;
+	public AudioSource sourceToPause;
+	public AudioSource sourceToPlayOnCollision;
+
+	void Start () {
+		sourceToPlayOnCollision = GetComponent<AudioSource>();
+	}
+
+	void OnTriggerEnter(Collider other) {
+
+		if (other.gameObject == objectToTrigger1 || other.gameObject == objectToTrigger2) {
+			sourceToPause.Pause();
+			sourceToPlayOnCollision.PlayOneShot(sourceToPlayOnCollision.clip);
+			Destroy(other.gameObject);
+			StartCoroutine(resumePausedAfterDelay(sourceToPlayOnCollision.clip.length));
+		}
+	}
+
+	IEnumerator resumePausedAfterDelay(float timeToWait)
+	{	
+		yield return new WaitForSeconds(timeToWait);
+		sourceToPause.Play();
+	}
+}
