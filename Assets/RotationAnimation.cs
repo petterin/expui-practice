24e7bbbfffeeaeb655ead2793da2cfaa31b1dc1a
Petteri Noponen <petteri.noponen@gmail.com>
2015-02-26 15:50:33 +0200

    Animate head when eating

--- Assets/RotationAnimation.cs
+++ Assets/RotationAnimation.cs
@@ -6,22 +6,50 @@ public class RotationAnimation : MonoBehaviour {
 	public int maxAngle = 40;
 	public int minAngle = -20;
 	public int rotationStep = 20;
+	public int timesToRepeat = 0; // 0 = infinite
+	public bool playing = true;
 
-	bool playing = false;
+	int halfRounds = 0;
 	float initialAngle = 0.0f;
+	bool returningToInitial = false;
+	float tolerance = 1.0f;
 	
 	// Animation initialization
 	void Start () {
-		playing = true;
 		initialAngle = transform.eulerAngles.z;
 	}
 
 	// Update is called once per frame
 	void Update () {
+		// Condition to finish
+		if (timesToRepeat > 0 && halfRounds >= 2*timesToRepeat) {
+			returningToInitial = true;
+		}
+
+		// Condition to stop
+		if (returningToInitial) {
+			if (Mathf.Abs(getAngleDifference()) <= tolerance) {
+				returningToInitial = false;
+				playing = false;
+				halfRounds = 0;
+			} else if (getAngleDifference() > 0) {
+				rotationStep = -Mathf.Abs(rotationStep);
+			} else if (getAngleDifference() < 0) {
+				rotationStep = Mathf.Abs(rotationStep);
+			}
+		}
+
+
 		if (playing) {
-			if ((getAngleDifference() < minAngle && rotationStep < 0) ||
-			    (getAngleDifference() > maxAngle && rotationStep > 0)) {
+			// Rotating
+			if ((!returningToInitial) &&
+				(
+					(getAngleDifference() < minAngle && rotationStep < 0) ||
+			    	(getAngleDifference() > maxAngle && rotationStep > 0)
+				)) {
 				rotationStep *= -1; // reverse rotation
+				halfRounds++; // One half animation round completed
+				//Debug.Log ("Animation (half) count is " + halfRounds);
 			}
 			transform.Rotate(new Vector3(0, 0, rotationStep) * Time.deltaTime);
 		}
---
+++ Assets/collisionHeadAnimation.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class collisionHeadAnimation : MonoBehaviour {
+
+	public GameObject objectToAnimate1 = null;
+	public GameObject objectToAnimate2 = null;
+	public int timesToAnimate = 1;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		if (objectToAnimate1 != null) {
+			Debug.Log (string.Format("Starting animations on {0}.", objectToAnimate1.name));
+			RotationAnimation anim = objectToAnimate1.GetComponent<RotationAnimation> ();
+			anim.playing = true;
+			anim.timesToRepeat = timesToAnimate;
+		}
+		if (objectToAnimate2 != null) {
+			Debug.Log (string.Format("Starting animations on {0}.", objectToAnimate2.name));
+			RotationAnimation anim = objectToAnimate2.GetComponent<RotationAnimation> ();
+			anim.playing = true;
+			anim.timesToRepeat = timesToAnimate;
+		}
+	}
+}
