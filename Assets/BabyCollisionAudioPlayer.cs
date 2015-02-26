1a452e67ff05185f1c183c7b83b4255b98b5a9a5
Petteri Noponen <petteri.noponen@gmail.com>
2015-02-26 05:58:08 +0200

    Scene with all elements, collision script with multiple targets, basic head animation.

--- Assets/BabyCollisionAudioPlayer.cs
+++ Assets/BabyCollisionAudioPlayer.cs
@@ -3,27 +3,34 @@ using System.Collections;
 
 public class BabyCollisionAudioPlayer : MonoBehaviour {
 
-	public GameObject objectToTrigger1;
-	public GameObject objectToTrigger2;
+	public string triggeringObjectPrefix;
 	public AudioSource sourceToPause;
 	public AudioSource sourceToPlayOnCollision;
+	public bool destroyObject = true;
+	public int pauseLength = 3;
 
 	void Start () {
 		sourceToPlayOnCollision = GetComponent<AudioSource>();
 	}
 
 	void OnTriggerEnter(Collider other) {
-
-		if (other.gameObject == objectToTrigger1 || other.gameObject == objectToTrigger2) {
+		//Debug.Log(string.Format("BabyCollisionAudioPlayer {0} was hit with {1}.", this.name, other.name));
+		if (other.name.StartsWith(triggeringObjectPrefix)) {
 			sourceToPause.Pause();
-			sourceToPlayOnCollision.PlayOneShot(sourceToPlayOnCollision.clip);
-			Destroy(other.gameObject);
-			StartCoroutine(resumePausedAfterDelay(sourceToPlayOnCollision.clip.length));
+			if (!sourceToPlayOnCollision.isPlaying) {
+				sourceToPlayOnCollision.Play();
+			}
+			if (destroyObject) {
+				//Debug.Log(string.Format("Deleting object {0}.", other.name));
+				Destroy(other.gameObject);
+			}
+			StartCoroutine(resumePausedAfterDelay(sourceToPlayOnCollision.clip.length + pauseLength));
 		}
 	}
 
 	IEnumerator resumePausedAfterDelay(float timeToWait)
-	{	
+	{
+		//Debug.Log(string.Format("BabyCollisionAudioPlayer {0} was paused for {1} secs.", this.name, timeToWait));
 		yield return new WaitForSeconds(timeToWait);
 		sourceToPause.Play();
 	}
---
+++ Assets/RotationAnimation.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationAnimation : MonoBehaviour {
+
+	public int maxAngle = 40;
+	public int minAngle = -20;
+	public int rotationStep = 20;
+
+	bool playing = false;
+	float initialAngle = 0.0f;
+	
+	// Animation initialization
+	void Start () {
+		playing = true;
+		initialAngle = transform.eulerAngles.z;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (playing) {
+			if ((getAngleDifference() < minAngle && rotationStep < 0) ||
+			    (getAngleDifference() > maxAngle && rotationStep > 0)) {
+				rotationStep *= -1; // reverse rotation
+			}
+			transform.Rotate(new Vector3(0, 0, rotationStep) * Time.deltaTime);
+		}
+	}
+
+	float getAngleDifference() {
+		// eulerAngles are 0...360, never negative or over 360
+		float diff = transform.eulerAngles.z - initialAngle;
+		if (diff > 180) {
+			return diff - 360;
+		} else {
+			return diff;
+		}
+	}
+
+}
